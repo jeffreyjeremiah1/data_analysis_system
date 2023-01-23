3f194a37bae23a32a1a3e3286e68ceaf93240091
Jeffrey Jeremiah <83784485+jeffreyjeremiah1@users.noreply.github.com>
2023-01-23 23:28:49 +0200

    new chart class

---
+++ charts.py
@@ -0,0 +1,61 @@
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Chart(object):
+
+    def __init__(self, data, frame, row, column, title, rotation=0):
+        self.data = data
+        self.frame = frame
+        self.title = title
+        self.row = row
+        self.column = column
+        self.rotation = rotation
+
+    def plot_chart(self, x_label, y_label):
+        # Style of chart
+        plt.style.use('fivethirtyeight')
+
+        # Set dimensions for graph
+        fig = plt.figure(figsize=(12, 8), dpi=30)
+
+        # plots the graph
+        ax = fig.add_subplot(111)
+        ax.plot(self.data)
+        ax.set_title(self.title, color='red',  fontsize=22)
+        ax.set_xlabel(x_label, color='red', fontsize=18)
+        ax.set_ylabel(y_label, color='red', fontsize=18)
+        plt.xticks(rotation=self.rotation)
+        canvasbar = FigureCanvasTkAgg(fig, master=self.frame)
+        fig.tight_layout()
+        canvasbar.draw()
+        canvasbar.get_tk_widget().grid(row=self.row, column=self.column)
+        plt.draw()
+
+    def pie_chart(self, legend_title):
+        # Plots a pie Chart
+        plt.style.use('fivethirtyeight')
+
+        fig, ax = plt.subplots(figsize=(12, 8), dpi=30, subplot_kw=dict(aspect="equal"))
+
+        data = self.data
+
+        chart_data = data.Total_case
+        chart_names = data.index
+
+        def func(pct, allvals):
+            absolute = int(np.round(pct / 100. * np.sum(allvals)))
+            return "{:.1f}%\n({:d} cases)".format(pct, absolute)
+
+        wedges, texts, autotexts = ax.pie(chart_data, autopct=lambda pct: func(pct, chart_data),
+                                          textprops=dict(color="w"))
+        ax.legend(wedges, chart_names, title=legend_title, loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+        plt.setp(autotexts, size=8, weight="bold")
+        ax.set_title(self.title, color='red', fontsize=24)
+        canvasbar = FigureCanvasTkAgg(fig, master=self.frame)
+        plt.xticks(rotation=self.rotation)
+        fig.tight_layout()
+        canvasbar.draw()
+        canvasbar.get_tk_widget().grid(row=self.row, column=self.column)
+        plt.draw()
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from police_data_module.stop_and_search import get_police_data
 from covid_19_module.covid19_data_ica import get_covid_data
+from charts import Chart
 
 # Gets data from the police data file
 police_data = get_police_data()
@@ -16,19 +17,16 @@ LARGE_FONT = ("verdana", 12)
 
 
 # Responsible for the homepage layout and navigation between pages
-class HomePage(tk.Tk):
 
+
+class HomePage(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-
-        tk.Tk.wm_title(self, "Sam's Project")
-
+        tk.Tk.wm_title(self, "Data Analysis System")
         container = tk.Frame(self, bg="black")
-
         self.geometry("1400x950")
         # self.geometry("%dx%d" % (HomePage.winfo_screenwidth(self), HomePage.winfo_screenheight(self)))
         self.resizable(False, False)
-
         container.pack(side="top", fill="both", expand=False)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -47,7 +45,6 @@ class HomePage(tk.Tk):
 
 
 class StartPage(tk.Frame):
-
     def __init__(self, parent, controller):
         super(StartPage, self).__init__()
         tk.Frame.__init__(self, parent, width=1080, height=750, bg="black")
@@ -55,27 +52,29 @@ class StartPage(tk.Frame):
         label.grid_rowconfigure(0, weight=1)
         label.grid_columnconfigure(0, weight=1)
 
-        self.backGroundImage = PhotoImage(file="images/background.png")
+        self.backGroundImage = PhotoImage(file="images/background1.png")
         self.backGroundImageLabel = Label(self, image=self.backGroundImage)
         self.backGroundImageLabel.pack()
 
-        self.canvas = Canvas(self, width=1080, height=700, bg="grey")
+        self.canvas = Canvas(self, width=1080, height=700, bg="#181C4E")
         self.canvas.place(x=150, y=60)
 
-        title = tk.Label(self, text="Welcome to the Sam's project Homepage", font=("Bold", 40), bg="grey")
-        title.place(x=355, y=100)
+        title = tk.Label(self, text="Welcome to the Homepage", font=("Bold", 40), bg="#181C4E", foreground='white')
+        title.place(x=450, y=100)
 
-        note1 = tk.Label(self, text="Click button for COVID-19 Confirmed Cases Module: ", font=("Arial", 18), bg="grey")
+        note1 = tk.Label(self, text="Click button for COVID-19 Confirmed Cases Module: ", font=("Arial", 18),
+                         bg="#181C4E", foreground='white')
         note1.place(x=200, y=250)
 
-        note2 = tk.Label(self, text="Click button for Stop and Search Module: ", font=("Arial", 18), bg="grey")
+        note2 = tk.Label(self, text="Click button for Stop and Search Module: ", font=("Arial", 18),
+                         bg="#181C4E", foreground='white')
         note2.place(x=200, y=300)
 
-        button1 = tk.Button(self, text="Covid-19 Cases", highlightbackground="grey", padx=2, pady=2, fg="#306998",
-                            bg="black", font=("Arial", 14), command=lambda: controller.show_frame(PageOne))
+        button1 = tk.Button(self, text="Covid-19 Cases", padx=2, pady=2, fg="#181C4E",
+                            bg="white", font=("Arial", 14), command=lambda: controller.show_frame(PageOne))
         button1.place(x=650, y=250)
 
-        button2 = tk.Button(self, text="Stop and Search", highlightbackground="grey", padx=2, pady=2, fg="#306998",
+        button2 = tk.Button(self, text="Stop and Search", highlightbackground="white", padx=2, pady=2, fg="#181C4E",
                             bg="white", font=("Arial", 14), command=lambda: controller.show_frame(PageTwo))
         button2.place(x=650, y=300)
 
@@ -97,122 +96,77 @@ class PageOne(tk.Frame):
                                                                                                   "Cases")
         header.grid(row=0, column=3, padx=(400, 470), pady=20)
 
+        # Question 1  ##################################################################
+
+        # Filters df by total number of cases each month
+        total_monthly_cases = covid_data.Total_case.groupby(covid_data.Month, sort=False).sum()
+
         tk.Label(frame, font=("Bold", 22), wraplength='1300', justify=LEFT,
                  text="1.  What are the total number of cases reported each month over from the start of reporting to "
                       "end ?").grid(row=0,
                                     column=0,
                                     sticky='W',
-                                    padx=0,
+                                    padx=50,
                                     pady=(50, 10))
 
-        def chart_1():
-            # Filters df by total number of cases each month
-            total_monthly_cases = covid_data.Total_case.groupby(covid_data.Month, sort=False).sum()
+        # Plots chart for question 1
+        chart_1 = Chart(row=1, column=0, data=total_monthly_cases, frame=frame,
+                        title='Total Number of Cases Reported vs Month')
+        chart_1.plot_chart(x_label='Month', y_label='Total Cases')
 
-            # Set dimensions for graph
-            fig = plt.figure(figsize=(12, 8), dpi=30)
+        # Question 2  ###################################################################
 
-            # plots the graph
-            ax = fig.add_subplot(111)
-            ax.plot(total_monthly_cases)
-            ax.set_title('Total Number of Cases Reported vs Month', fontsize=22, color='red')
-            ax.set_xlabel('Month', color='red')
-            ax.set_ylabel('Total Cases', color='red')
-            plt.xticks(rotation=90)
-            canvasbar = FigureCanvasTkAgg(fig, master=frame)
-            canvasbar.draw()
-            canvasbar.get_tk_widget().grid(row=1, column=0, padx=0)
-            plt.draw()
+        # Select first and last day of september as a date range
+        start_date = '2020-09-01'
+        end_date = '2020-09-30'
+
+        # Filter the DataFrame for the month of september
+        df_september = covid_data[(covid_data['date'] >= start_date) & (covid_data['date'] <= end_date)]
+        total_daily_cases_september = df_september.Total_case.groupby(df_september['date'], sort=False).sum()
 
         tk.Label(frame, font=("Bold", 22), wraplength='1000', justify=LEFT,
                  text="2. What are Total number of cases reported each day over a given date range ? ").grid(row=2,
                                                                                                              column=0,
                                                                                                              sticky='W',
-                                                                                                             padx=0,
+                                                                                                             padx=50,
                                                                                                              pady=(
                                                                                                                  50,
                                                                                                                  10))
+        # Plots chart for question 1
+        chart_2 = Chart(row=3, column=0, data=total_daily_cases_september, frame=frame,
+                        title='Daily cases for the month of september', rotation=90)
+        chart_2.plot_chart(x_label='Date', y_label='Total Cases')
 
-        def chart_2():
-            # Select first and last day of september as a date range
-            start_date = '2020-09-01'
-            end_date = '2020-09-30'
+        # Question 3  #######################################################################
 
-            # Filter the DataFrame for the month of september
-            df_september = covid_data[(covid_data['date'] >= start_date) & (covid_data['date'] <= end_date)]
-            total_daily_cases_september = df_september.Total_case.groupby(df_september['date'], sort=False).sum()
+        # Chosen date
+        date = '2020-04-01'
 
-            # Plots the graph
-            fig = plt.figure(figsize=(12, 8), dpi=30)
+        # Filters the DataFrame to include only the rows that correspond to the given day.
+        df_by_date = covid_data.query(f'date == "{date}"')
 
-            ax = fig.add_subplot(111)
-            ax.plot(total_daily_cases_september.index, total_daily_cases_september.values)
+        # Groups the DataFrame by the area column and makes 'areaName' index.
+        df_by_area = df_by_date.groupby(df_by_date['areaName']).count()
 
-            ax.set_title('Daily cases for the month of september', fontsize=24, color='red')
-            ax.set_xlabel('date', color='red', fontsize=10)
-            ax.set_ylabel('Total Cases', color='red')
-            ax.xaxis.set_tick_params(pad=30)
-            plt.xticks(rotation=90)
-            canvasbar = FigureCanvasTkAgg(fig, master=frame)
-            # Adjust the size of the plot to fit the labels
-            fig.tight_layout()
-            canvasbar.draw()
-            canvasbar.get_tk_widget().grid(row=3, column=0, padx=0)
-            plt.draw()
+        # finds the top n areas with the highest number of cases
+        top_areas = df_by_area.nlargest(5, 'Total_case')
 
         tk.Label(frame, font=("Bold", 22), wraplength='1300', justify=LEFT,
-                 text="3. What are the areas with the highest number of cases on a given day ?").grid(row=4,
-                                                                                                      column=0,
-                                                                                                      sticky='W',
-                                                                                                      padx=0,
-                                                                                                      pady=(50, 10))
+                 text="3. What are the areas with the highest number of cases on a given day ?") \
+            .grid(row=4, column=0, sticky='W', padx=50, pady=(50, 10))
 
-        def chart_3():
-            # Chosen date
-            date = '2020-04-01'
+        chart_3 = Chart(row=5, column=0, data=top_areas, frame=frame,
+                        title=f"Top 5 Areas with the highest number of cases on {date}")
+        chart_3.pie_chart(legend_title='Area Names')
 
-            # Filters the DataFrame to include only the rows that correspond to the given day.
-            df_by_date = covid_data.query(f'date == "{date}"')
-
-            # Groups the DataFrame by the area column and makes 'areaName' index.
-            df_by_area = df_by_date.groupby(df_by_date['areaName']).count()
-
-            # finds the top n areas with the highest number of cases
-            top_areas = df_by_area.nlargest(5, 'Total_case')
-
-            #  Plots a pie Chart
-            plt.style.use('fivethirtyeight')
-
-            fig, ax = plt.subplots(figsize=(12, 8), dpi=30, subplot_kw=dict(aspect="equal"))
-
-            data = top_areas.Total_case
-            area_names = top_areas.index
-
-            def func(pct, allvals):
-                absolute = int(np.round(pct / 100. * np.sum(allvals)))
-                return "{:.1f}%\n({:d} cases)".format(pct, absolute)
-
-            wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
-                                              textprops=dict(color="w"))
-            ax.legend(wedges, area_names,
-                      title="Area Names",
-                      loc="center left",
-                      bbox_to_anchor=(1, 0, 0.5, 1))
-
-            plt.setp(autotexts, size=8, weight="bold")
-            ax.set_title(f"Top 5 Areas with the highest number of cases on {date}", color='red', fontsize=24)
-            canvasbar = FigureCanvasTkAgg(fig, master=frame)
-            plt.xticks(rotation=30)
-            canvasbar.draw()
-            canvasbar.get_tk_widget().grid(row=5, column=0, padx=0)
-            plt.draw()
+        # Question 4  #####################################################################
 
         tk.Label(frame, font=("Bold", 22), wraplength='1300', justify=LEFT,
                  text="4. Are there any comparison between two or more areas concerning their cumulative cases total "
                       "over a given date range ?").grid(row=6,
                                                         column=0,
                                                         sticky='W',
-                                                        padx=0,
+                                                        padx=50,
                                                         pady=(50, 10))
 
         def chart_4():
@@ -246,13 +200,13 @@ class PageOne(tk.Frame):
             ax.plot(area1_cases, label=areas[0])
             ax.plot(area2_cases, label=areas[1])
             ax.plot(area3_cases, label=areas[2])
-            ax.set_xlabel('Date', color='green')
-            ax.set_ylabel('Cumulative Cases Total', color='green')
-            ax.set_title(f'Cumulative Cases Total by Area from {start_date} to {end_date}', color='red', fontsize=23)
+            ax.set_xlabel('Date', color='green', fontsize=18)
+            ax.set_ylabel('Cumulative Cases Total', color='green', fontsize=18)
+            ax.set_title(f'Cumulative Cases Total by Area from {start_date} to {end_date}', color='red', fontsize=22)
             # plt.xticks(rotation=45)
             # Add padding to the x-tick labels
             ax.xaxis.set_tick_params(pad=30)
-
+            fig.tight_layout()
             ax.legend(areas)
             canvasbar = FigureCanvasTkAgg(fig, master=frame)
             canvasbar.draw()
@@ -274,10 +228,8 @@ class PageOne(tk.Frame):
             main_container.pack(fill=tk.BOTH, side=tk.LEFT, expand=tk.TRUE)
             main_container.create_window(0, 0, window=frame, anchor=tk.NW)
 
-        chart_1()
-        chart_2()
-        chart_3()
         chart_4()
+
         create_scroll_bar_container()
         update_scrolling_region()
 
@@ -325,7 +277,7 @@ class PageTwo(tk.Frame):
                  text="1.  What are the top 5 age range with the most arrests ?").grid(row=0,
                                                                                        column=0,
                                                                                        sticky='W',
-                                                                                       padx=0,
+                                                                                       padx=50,
                                                                                        pady=(50, 10))
 
         def chart_1():
@@ -361,6 +313,7 @@ class PageTwo(tk.Frame):
             ax.set_title(f"Top 5 Age Ranges with highest Arrests", color='red', fontsize=24)
             canvasbar = FigureCanvasTkAgg(fig, master=frame)
             canvasbar.draw()
+            fig.tight_layout()
             canvasbar.get_tk_widget().grid(row=1, column=0, padx=0)
             plt.draw()
 
@@ -368,7 +321,7 @@ class PageTwo(tk.Frame):
                  text="2. What are the top locations where interactions takes place ?").grid(row=2,
                                                                                              column=0,
                                                                                              sticky='W',
-                                                                                             padx=0,
+                                                                                             padx=50,
                                                                                              pady=(50, 10))
 
         def chart_2():
@@ -387,8 +340,8 @@ class PageTwo(tk.Frame):
 
             # Add a title and labels to the chart
             ax.set_title('Top 10 Location names of interactions ', fontsize=22, color='red')
-            ax.set_xlabel('Location ', color='red')
-            ax.set_ylabel('Interaction Count', color='red')
+            ax.set_xlabel('Location ', color='red', fontsize=18)
+            ax.set_ylabel('Interaction Count', color='red', fontsize=18)
             plt.xticks(rotation=90)
             # ax.xaxis.set_tick_params(pad=20)
             fig.tight_layout()
@@ -401,7 +354,7 @@ class PageTwo(tk.Frame):
                  text="3. What are the top locations where interactions takes place ?").grid(row=4,
                                                                                              column=0,
                                                                                              sticky='W',
-                                                                                             padx=0,
+                                                                                             padx=50,
                                                                                              pady=(50, 10))
 
         def chart_3():
@@ -422,13 +375,14 @@ class PageTwo(tk.Frame):
 
             # Add a title and labels to the chart
             ax.set_title('Ethnicity Count by Outcome', color='red', fontsize=24)
-            ax.set_xlabel('Ethnicity (self_defined_ethnicity, officer_defined_ethnicity)', color='red')
-            ax.set_ylabel('Count', color='red')
+            ax.set_xlabel('Ethnicity (self_defined_ethnicity, officer_defined_ethnicity)', color='red', fontsize=18)
+            ax.set_ylabel('Count', color='red', fontsize=18)
             # Add a legend to the chart
             plt.legend(ethnicity_count, title='Outcome')
             plt.xticks(rotation=0)
             canvasbar = FigureCanvasTkAgg(fig, master=frame)
             canvasbar.draw()
+            fig.tight_layout()
             canvasbar.get_tk_widget().grid(row=5, column=0, padx=0, pady=(0, 100))
             plt.draw()
 
@@ -436,7 +390,7 @@ class PageTwo(tk.Frame):
                  text="4. Are there any patterns in the legislation based on gender ?").grid(row=6,
                                                                                              column=0,
                                                                                              sticky='W',
-                                                                                             padx=0,
+                                                                                             padx=50,
                                                                                              pady=(50, 10))
 
         def chart_4():
@@ -455,14 +409,15 @@ class PageTwo(tk.Frame):
 
             # Add a title and labels to the chart
             ax.set_title('Legislation Count Based on Gender', color='red', fontsize=24)
-            ax.set_xlabel('Legislation ', color='red')
-            ax.set_ylabel('Count', color='red')
+            ax.set_xlabel('Legislation ', color='red', fontsize=18)
+            ax.set_ylabel('Count', color='red', fontsize=18)
             plt.xticks(rotation=0)
 
             # Add a legend to the chart
             plt.legend(legislation_count, title='Gender')
             canvasbar = FigureCanvasTkAgg(fig, master=frame)
             canvasbar.draw()
+            fig.tight_layout()
             canvasbar.get_tk_widget().grid(row=7, column=0, padx=0, pady=(0, 400))
             plt.draw()
 
